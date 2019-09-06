0b7bb794de8df83900b53d570e5b26fb29cc484a
Kristin Barr <kristinbarr@gmail.com>
2019-09-06 14:56:27 -0400

    abstractDeposit cleanup

--- src/piggybank/AbstractDeposit.java
+++ src/piggybank/AbstractDeposit.java
@@ -1,14 +1,12 @@
 package piggybank;
 
 public abstract class AbstractDeposit {
-// create the deposit totals
 
   int amount;
   double depositTotal;
 
   public AbstractDeposit(int amount) {
     this.amount = amount;
-    // this.depositTotal = depositTotal;
   }
 
   public AbstractDeposit() {
@@ -26,13 +24,7 @@ public abstract class AbstractDeposit {
   }
 
   public abstract void deposit(int amount);
-  // public void deposit(int amount) {
-  //   depositTotal = amount * getValue();
-  // }
 
   public abstract double getDepositTotal();
-  // public double getDepositTotal() {
-  //   return depositTotal;
-  // }
 
 }
