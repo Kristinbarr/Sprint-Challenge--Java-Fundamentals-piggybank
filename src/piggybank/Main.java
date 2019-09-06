4b6dc46e7595544e41946f15c969b58b30f15430
Kristin Barr <kristinbarr@gmail.com>
2019-09-06 14:33:25 -0400

    testing bank contents, updated total print logic

--- src/piggybank/Main.java
+++ src/piggybank/Main.java
@@ -5,31 +5,45 @@ import java.util.*;
 
 public class Main {
 
-  // public static void printPiggybank(ArrayList<AbstractAnimal> animals, CheckAnimal tester) {
-  //   for (AbstractAnimal a : animals) {
-  //     if (tester.test(a)) {
-  //       System.out.println(
-          // "The Piggy Bank holds " + fp.format(myValue) +
-  //           "Name: " + a.getName() + ", species: " + a.getClass().getSimpleName() + ", named in: " + a.getYear() + ", movement: " + a.move());
-  //     }
-  //   }
-  // }
+
+  public static void printPiggybank(ArrayList<AbstractDeposit> deposits) {
+    DecimalFormat fp = new DecimalFormat("$###,###.00");
+
+    double myValue = 0;
+
+    for (AbstractDeposit d : deposits) {
+
+      myValue = myValue + d.getDepositTotal();
+    }
+    System.out.println("The Piggy Bank holds " + fp.format(myValue));
+  }
 
   public static void main(String[] args) {
 
-    DecimalFormat fp = new DecimalFormat("$###,###.00");
+    // ______printing out list of coins and their deposit amounts______
 
-    CreateCollection total = new CreateCollection();
-
-    // HashMap<String, Integer> piggyBank = new HashMap<String, Integer>();
-    piggyBank.add(new Quarter());
-    piggyBank.add(new Dime());
-    piggyBank.add(new Dollar(5));
-    piggyBank.add(new Nickel(3));
+    // arraylist will hold [value, amount]?
+    ArrayList<AbstractDeposit> piggyBank = new ArrayList<AbstractDeposit>();
+    // piggyBank.add(new Quarter());
+    piggyBank.add(new Dime(2));
+    // piggyBank.add(new Dollar(5));
+    // piggyBank.add(new Nickel(3));
     piggyBank.add(new Dime(7));
-    piggyBank.add(new Dollar());
-    piggyBank.add(new Penny(10));
+    // piggyBank.add(new Dollar());
+    // piggyBank.add(new Penny(10));
 
+    System.out.println();
+    System.out.println("___amounts___");
+    piggyBank.forEach((c) -> System.out.println(c.getAmount()));
+    System.out.println("___values___");
+    piggyBank.forEach((c) -> System.out.println(c.getValue()));
+    System.out.println("___depositTotals___");
+    piggyBank.forEach((c) -> System.out.println(c.getDepositTotal()));
+    System.out.println("___piggybank contents___");
+    piggyBank.forEach((c)-> System.out.println(c));
+
+
+    printPiggybank(piggyBank);
 
   }
 
